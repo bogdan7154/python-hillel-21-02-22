94fcb31811a9a8468e077d7774802502dd9c0d13
bogdan7154 <bogdan7154@ukr.net>
2023-02-26 00:07:31 +0200

    1/4 dz

--- maiin.py
+++ maiin.py
@@ -1,8 +1,11 @@
+# узнаем данные от пользователя
 gasoline_consumption = float(input("Введите расход на 100км: "))
 gasoline_price = float(input("Введите цену 1л бензина: "))
 distance = float(input("Введите растояние которое нужно проехать: "))
 
+# вычисляем сколько уйдет бенза и стоимость
 total_consumption = gasoline_consumption * distance / 100
 result = total_consumption * gasoline_price
 
+# даем понять что нужно покупать электрокар))
 print("Витрати на паливо: {:.2f} грн".format(result))
