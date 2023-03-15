cf7581a2989d634c61e2e4ea1d461bfee086c1ad
bogdan7154 <bogdan7154@ukr.net>
2023-03-15 17:53:09 +0200

    removing brackets from text

--- bot_one.py
+++ bot_one.py
@@ -9,14 +9,3 @@ while True:
         numbers.append(number)
     except ValueError:  # а вот если нет
         print("Некоректне введення. Введіть число або 'sum' для підсумовування.")
-x = [1, 5, 8, 9, 10]
-print(x[::-1])
-
-
-text = input('Введите текст со скобками: ')
-while '(' in text:
-    beginning_of_text = text.find('(')
-    end_of_text = text.find(')')
-    text = text[:beginning_of_text] + text[end_of_text+1:]
-
-print(text)
