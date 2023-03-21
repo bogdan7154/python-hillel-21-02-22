2beeb91de89f4b2f40fe4d5aebae42971959c20e
bogdan7154 <bogdan7154@ukr.net>
2023-03-21 16:22:52 +0200

    2

--- triangle.py
+++ triangle.py
@@ -1,27 +1,43 @@
-# импортируем библиотеку
-import math
-
-# создаем в функции цыкл ввода с обработкой не правильного значения
+# функция для ввода сторон треугольника пользователем с обработкой ошибок ввода
 def reading_values(comment):
     while True:
-        num = float(input(comment))
-        if num <= 0:
-            print('Введите положительное  число')
+        num = input(comment)
+        if num.isdigit() and float(num) > 0:
+            return float(num)
         else:
-            return num
+            print('Введите положительное число')
+
 
 # создаем функцию где проверяем треугольник на правильность
 def triangle_existence_test(a, b, c):
     return (a + b > c) and (a + c > b) and (b + c > a)
 
+
 # функция где вычисляем периметр
-def calculate_perimetr(a, b, c):
+def calculate_perimeter(a, b, c):
     return a + b + c
 
-# функция где вычисляем площадь
+
+# функция для вычисления площади треугольника
 def calculate_area(a, b, c):
-    p = calculate_perimetr(a, b, c) / 2
-    return math.sqrt(p * (p - a) * (p - b) * (p - c))
+    p = calculate_perimeter(a, b, c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+# функция для определения типа треугольника
+def get_triangle_type(a, b, c):
+    if a == b == c:
+        return 'равносторонний'
+    elif a == b or b == c or a == c:
+        if a ** 2 + b ** 2 == c ** 2 or b ** 2 + c ** 2 == a ** 2 or a ** 2 + c ** 2 == b ** 2:
+            return 'прямоугольный'
+        else:
+            return 'равнобедренный'
+    elif a ** 2 + b ** 2 == c ** 2 or b ** 2 + c ** 2 == a ** 2 or a ** 2 + c ** 2 == b ** 2:
+        return 'прямоугольный'
+    else:
+        return 'обычный'
+
 
 # диалог с пользователем. Узнаем значение сторон
 if __name__ == '__main__':
@@ -29,10 +45,12 @@ if __name__ == '__main__':
     b = reading_values('Введите вторую сторону треугольника: ')
     c = reading_values('Введите третью сторону треугольника: ')
 
-# сохраняем в переменную результаты цыклов  и выводим результат
-    if calculate_perimetr(a, b, c):
-        perimetr = calculate_perimetr(a, b, c)
+    # проверяем наличие треугольника и выводим результат
+    if triangle_existence_test(a, b, c):
+        perimeter = calculate_perimeter(a, b, c)
         area = calculate_area(a, b, c)
-        print(f'треугольник со сторонами {a},{b},{c} существует. Его периметр равен {perimetr:.2f}, а площадь {area:.2f}')
+        triangle_type = get_triangle_type(a, b, c)
+        print(f'Треугольник со сторонами {a}, {b}, {c} является {triangle_type}. Его периметр равен {perimeter:.2f}'
+              f', а площадь {area:.2f}')
     else:
-        print(f'треугольника со сторонами {a},{b},{c} не существует ')
+        print(f'Треугольника со сторонами {a}, {b}, {c} не существует')
