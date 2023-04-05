af1e6b6e85bc1f3d3ba97d8754f93063a7cb966a
bogdan7154 <bogdan7154@ukr.net>
2023-04-05 20:14:51 +0300

    1

---
+++ open_file/redactor_file.py
@@ -0,0 +1,5 @@
+with open('my_text.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines() if 'a' in line.lower()]
+    result = [line[line.index('a'):].strip().capitalize() if 'a' in line else '' for line in lines]
+
+print(result)
